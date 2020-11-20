94fe3fa27c602b7563a5a39cac866c4fca4523ee
mykola holovko <Holovko.mykola@gmail.com>
2020-11-20 05:59:25 +0200

    solvePnP

--- solvePnP/__init__.py
+++ solvePnP/__init__.py
@@ -1,24 +1,38 @@
 import logging
 
 import azure.functions as func
-
+import json
+import base64
+from src.Camera import Camera
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    data = req.params.get('data')
+
+    if not data:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            data = req_body.get('data')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if data:
+        data = json.loads(base64.b64decode(data).decode('utf-8'))
+        points = data['points']
+        
+        camera = Camera(points)
+        camera_position,rotM = camera.camera_position()
+
+        output = dict()
+        output['rotM'] = str(rotM)
+        output['camera_position'] = str(camera_position)
+
+        return func.HttpResponse(f"{output}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
